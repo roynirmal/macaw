361ceccffd90d1fd4eb446dcead298419ae2a477
roynirmal <nroy000@gmail.com>
2020-04-09 17:28:43 +0200

    working bare bone version of macaw

--- macaw/live_main.py
+++ macaw/live_main.py
@@ -5,7 +5,7 @@ Authors: Hamed Zamani (hazamani@microsoft.com)
 """
 
 from macaw.cis import CIS
-from macaw.core import mrc, retrieval
+from macaw.core import retrieval
 from macaw.core.input_handler.action_detection import RequestDispatcher
 from macaw.core.output_handler import naive_output_selection
 from macaw.util.logging import Logger
@@ -27,8 +27,9 @@ class ConvQA(CIS):
         self.logger = params['logger']
         self.logger.info('Conversational QA Model... starting up...')
         self.retrieval = retrieval.get_retrieval_model(params=self.params)
-        self.qa = mrc.get_mrc_model(params=self.params)
-        self.params['actions'] = {'retrieval': self.retrieval, 'qa': self.qa}
+        # self.qa = mrc.get_mrc_model(params=self.params)
+        # self.params['actions'] = {'retrieval': self.retrieval, 'qa': self.qa}
+        self.params['actions'] = {'retrieval': self.retrieval}
         self.request_dispatcher = RequestDispatcher(self.params)
         self.output_selection = naive_output_selection.NaiveOutputProcessing({})
 
@@ -68,20 +69,20 @@ if __name__ == '__main__':
                  'interaction_db_name': 'macaw_test'}
 
     # These are interface parameters. They are interface specific.
-    interface_params = {'interface': 'telegram',  # interface can be 'telegram' or 'stdio' for live mode, and 'fileio'
-                                                  # for exp mode.
-                        'bot_token': 'YOUR_TELECGRAM_BOT_TOKEN',  # Telegram bot token.
-                        'asr_model': 'google',  # The API used for speech recognition.
-                        'asg_model': 'google',  # The API used for speech generation.
-                        'google-speech-to-text-credential-file': 'YOUR_GOOGLE_CREDENTIAL_FILE'}
-
+    # interface_params = {'interface': 'stdio',  # interface can be 'telegram' or 'stdio' for live mode, and 'fileio'
+    #                                               # for exp mode.
+    #                     'bot_token': 'YOUR_TELECGRAM_BOT_TOKEN',  # Telegram bot token.
+    #                     'asr_model': 'google',  # The API used for speech recognition.
+    #                     'asg_model': 'google',  # The API used for speech generation.
+    #                     'google-speech-to-text-credential-file': 'YOUR_GOOGLE_CREDENTIAL_FILE'}
+    interface_params = {'interface': 'stdio'}
     # These are parameters used by the retrieval model.
     retrieval_params = {'query_generation': 'simple',  # the model that generates a query from a conversation history.
                         'use_coref': True,  # True, if query generator can use coreference resolution, otherwise False.
-                        'search_engine': 'bing',  # the search engine. It can be either 'indri' or 'bing'.
+                        'search_engine': 'indri',  # the search engine. It can be either 'indri' or 'bing'.
                         'bing_key': 'YOUR_BING_SUBSCRIPTION_KEY',  # Bing API key
-                        'search_engine_path': 'PATH_TO_INDRI',  # The path to the indri toolkit.
-                        'col_index': 'PATH_TO_INDRI_INDEX',  # The path to the indri index.
+                        'search_engine_path': '/indri-5.11/',  # The path to the indri toolkit.
+                        'col_index': '/data/index/',  # The path to the indri index.
                         'col_text_format': 'trectext',  # collection text format. Standard 'trectext' is only supported.
                         'results_requested': 3}  # Maximum number of docs that should be retrieved by search engine.
     # Note: If you want to have a re-ranking model (e.g., learning to rank), you just need to simply extend the class
@@ -91,13 +92,14 @@ if __name__ == '__main__':
     # 'get_results' in class core.retrieval.search_engine.Retrieval.
 
     # These are parameters used by the machine reading comprehension model.
-    mrc_params = {'mrc': 'drqa',  # MRC model.
-                  'mrc_model_path': 'PATH_TO_PRETRAINED_MRC_MODEL',  # The path to the model parameters.
-                  'mrc_path': 'PATH_TO_MRC_DIRECTORY',  # The path to the model toolkit.
-                  'corenlp_path': 'PATH_TO_STANFORD_CORE_NLP_DIRECTORY',  # The path to the corenlp toolkit.
-                  'qa_results_requested': 3}  # The number of candidate answers returned by the MRC model.
+    # mrc_params = {'mrc': 'drqa',  # MRC model.
+    #               'mrc_model_path': 'PATH_TO_PRETRAINED_MRC_MODEL',  # The path to the model parameters.
+    #               'mrc_path': 'PATH_TO_MRC_DIRECTORY',  # The path to the model toolkit.
+    #               'corenlp_path': 'PATH_TO_STANFORD_CORE_NLP_DIRECTORY',  # The path to the corenlp toolkit.
+    #               'qa_results_requested': 3}  # The number of candidate answers returned by the MRC model.
 
-    params = {**basic_params, **db_params, **interface_params, **retrieval_params, **mrc_params}
+    # params = {**basic_params, **db_params, **interface_params, **retrieval_params, **mrc_params}
+    params = {**basic_params, **db_params, **interface_params, **retrieval_params}
     basic_params['logger'].info(params)
     ConvQA(params).run()
 
